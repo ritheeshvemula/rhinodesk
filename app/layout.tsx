461a85ba521246efe5c24aa70e0fb344037ddb56
CS <cstangirala2003@gmail.com>
2025-10-15 16:04:49 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Montserrat  } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 
 import "./globals.css";
 
@@ -40,6 +41,7 @@ export default function RootLayout({
         <body className={`${montserrat.variable} antialiased`}>
       
         {children}
+         <Analytics />
       </body>
     </html>
   );
